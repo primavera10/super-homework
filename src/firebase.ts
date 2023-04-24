1a3ffcca2b98568783b5f419101f8a1acd001cf8
Liza <lizavetayalak2000@gmail.com>
2023-04-24 23:51:21 +0200

    add firebase, add VueFire

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDM8f8ZaubFUqdl3TNCXy4V6pnRfHywgCE",
+    authDomain: "super-homework.firebaseapp.com",
+    projectId: "super-homework",
+    storageBucket: "super-homework.appspot.com",
+    messagingSenderId: "936889458041",
+    appId: "1:936889458041:web:e105d95751682e185be3a8",
+    measurementId: "G-VBEEX8Z86W"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { VueFire, VueFireAuth } from "vuefire";
+import { firebaseApp } from "@/firebase";
 
 const app = createApp(App)
 
+
 app.use(router)
+app.use(VueFire,{
+    firebaseApp,
+    modules :[
+        VueFireAuth(),
+    ]
+})
 
 app.mount('#app')
