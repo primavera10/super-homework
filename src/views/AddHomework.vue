ade5a593bade57bb7a41a59c30e6c1b21086fc6d
Liza <lizavetayalak2000@gmail.com>
2023-05-07 16:45:56 +0200

    feat : add DayJs

--- src/views/AddHomework.vue
+++ src/views/AddHomework.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="container bg-white">
-        <div class="text-center text-3xl font-bold text-primary mt-12" v-if="event">
-            Homework for
-            <span class="text-cyan"> {{ event.date }} </span>
+        <div class="text-3xl font-bold text-primary mt-12">
+            Homework
+        </div>
+        <div class="text-darkGray mt-3 text-lg" v-if="event">
+            Valid until {{ dayjs(event.date).format('DD/MM/YYYY HH:mm') }}
         </div>
     </div>
 </template>
@@ -13,6 +15,7 @@
     import { getDoc, doc } from "firebase/firestore";
     import { db, eventConverter, type CalendarEvent } from "@/firebase";
     import { ref } from "vue";
+    import * as dayjs from 'dayjs'
 
     const event = ref<CalendarEvent>();
 
