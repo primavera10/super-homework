aa5e2c09449a67a4179a5cc6573f64fbc513e6c8
Liza <lizavetayalak2000@gmail.com>
2023-05-07 16:23:35 +0200

    feat : get formatted data from firestore

--- src/views/AddHomework.vue
+++ src/views/AddHomework.vue
@@ -1,28 +1,28 @@
 <template>
-<div  class="container bg-white">
-    <div>
-        {{route.params.id}}
+    <div class="container bg-white">
+        <div class="text-center text-3xl font-bold text-primary mt-12" v-if="event">
+            Homework for
+            <span class="text-cyan"> {{ event.date }} </span>
+        </div>
     </div>
-</div>
 </template>
 
 <script setup lang="ts">
 
-    import {useRoute} from "vue-router";
-    import {getDoc, doc} from "firebase/firestore";
-    import { db } from "@/firebase";
+    import { useRoute } from "vue-router";
+    import { getDoc, doc } from "firebase/firestore";
+    import { db, eventConverter, type CalendarEvent } from "@/firebase";
     import { ref } from "vue";
 
-    const event = ref();
+    const event = ref<CalendarEvent>();
+
     const route = useRoute();
     const id = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id
 
-     getDoc(doc(db,'events', id)).then(e => {
-         event.value = e.data()
-     })
-
-
-
+    getDoc(doc(db, 'events', id).withConverter(eventConverter))
+        .then(e => {
+            event.value = e.data()
+        });
 
 
 </script>
