2763ebcaafeadb204d31950354b6d1335c9f904b
Liza <lizavetayalak2000@gmail.com>
2023-05-10 18:59:04 +0200

    feat : add cloud storage, add unique id's for files while uploading, add uploading logic

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 import { getFirestore, type DocumentData, type QueryDocumentSnapshot, type SnapshotOptions } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDM8f8ZaubFUqdl3TNCXy4V6pnRfHywgCE",
     authDomain: "super-homework.firebaseapp.com",
     projectId: "super-homework",
-    storageBucket: "super-homework.appspot.com",
+    storageBucket: "gs://super-homework.appspot.com",
     messagingSenderId: "936889458041",
     appId: "1:936889458041:web:e105d95751682e185be3a8",
-    measurementId: "G-VBEEX8Z86W"
+    measurementId: "G-VBEEX8Z86W",
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
@@ -42,4 +43,5 @@ export const eventConverter = {
         } as CalendarEvent;
     }
 }
+export const storage = getStorage(firebaseApp);
 
--- src/views/AddHomework.vue
+++ src/views/AddHomework.vue
@@ -1,10 +1,35 @@
 <template>
-    <div class="container bg-white">
+    <div v-if="event && user" class="container bg-white">
         <div class="text-3xl font-bold text-primary mt-12">
             Homework
         </div>
-        <div class="text-darkGray mt-3 text-lg" v-if="event">
+        <div class="text-red mt-3 text-lg">
             Valid until {{ dayjs(event.date).format('DD/MM/YYYY HH:mm') }}
+        </div>
+        <div class="text-darkGray mt-2 ">
+            Created by {{ event.createdBy.email }} at {{ dayjs(event.createdAt).format('DD/MM/YYYY') }}
+        </div>
+        <div class="mt-8 text-2xl font-bold">
+            Name: {{ event.title }}
+        </div>
+        <div class="mt-4 text-lg">
+            Message: {{ event.message }}
+        </div>
+        <div class="mt-10">
+            <div class="text-2xl font-bold">
+                Your answer:
+            </div>
+            <div class="flex gap-4">
+                <textarea class="border-lightGray rounded-lg p-2 mt-4 border w-full focus:outline-primary"
+                          v-model="answer"/>
+                <input type="file" ref="fileInput">
+            </div>
+            <div class="mt-6 text-red">
+                Please either upload a file or write a message
+            </div>
+            <div @click="submitAnswer" class="mt-12 w-fit p-3 rounded-2xl bg-primary hover:bg-cyan text-white">
+                Submit
+            </div>
         </div>
     </div>
 </template>
@@ -12,12 +37,18 @@
 <script setup lang="ts">
 
     import { useRoute } from "vue-router";
-    import { getDoc, doc } from "firebase/firestore";
+    import { getDoc, doc, setDoc } from "firebase/firestore";
     import { db, eventConverter, type CalendarEvent } from "@/firebase";
+    import { useCurrentUser } from 'vuefire'
     import { ref } from "vue";
     import * as dayjs from 'dayjs'
+    import { getStorage, ref as fileRef, uploadBytes } from "firebase/storage";
+    import { v4 as uuidv4 } from 'uuid';
+
 
     const event = ref<CalendarEvent>();
+    const fileInput = ref()
+    const user = useCurrentUser();
 
     const route = useRoute();
     const id = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id
@@ -26,6 +57,54 @@
         .then(e => {
             event.value = e.data()
         });
+
+    const answer = ref('');
+    const storage = getStorage();
+    const errorMessage = ref(false);
+
+    async function submitAnswer() {
+        if (answer.value && fileInput) {
+            const documentRef = fileRef(storage, `${uuidv4()}`);
+            errorMessage.value = false;
+            uploadBytes(documentRef, fileInput.value.files[0]).then((snapshot) => {
+                console.log('Uploaded a blob or file!')
+            })
+            await setDoc(doc(db, "events", id), {
+                answers: [{
+                    addedAt: Date.now(),
+                    message: answer.value,
+                    documentLink: documentRef,
+                    email: user.value!.email,
+                }
+                ]
+            })
+        } else if (fileInput.value && !answer.value) {
+            errorMessage.value = false;
+            const documentRef = fileRef(storage, `${uuidv4()}`);
+            uploadBytes(documentRef, fileInput.value.files[0]).then((snapshot) => {
+                console.log('Uploaded a blob or file!')
+            })
+            await setDoc(doc(db, "events", id), {
+                answers: [{
+                    addedAt: Date.now(),
+                    documentLink: documentRef,
+                    email: user.value!.email,
+                }
+                ]
+            })
+        } else if (answer.value && !fileInput.value) {
+            errorMessage.value = false;
+            await setDoc(doc(db, "events", id), {
+                answers: [{
+                    addedAt: Date.now(),
+                    message: answer.value,
+                    email: user.value!.email,
+                }]
+            })
+        } else {
+            return errorMessage.value = true
+        }
+    }
 
 
 </script>
